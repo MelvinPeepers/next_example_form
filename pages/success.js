acbc7e8dd8761a168dc771d0d5d38a8080ad68a9
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2022-07-19 12:06:50 -0400

    added toml

---
+++ pages/success.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Success() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Toolbox Template!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Submission Received!</h1>
+        <p>Thank you for your feedback! Head back to the <Link href="/"><a>home page</a></Link>.</p>
+      </main>
+    </div>
+  )
+}
